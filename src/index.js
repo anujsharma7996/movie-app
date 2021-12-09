d5ba580a194ddb451126db41b849e63684e512b6
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-12-09 15:33:46 +0530

    Added store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
 import "./index.css";
 import App from "./components/App";
+import movies from "./reducers";
+
+const store = createStore(movies);
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById("root"));
